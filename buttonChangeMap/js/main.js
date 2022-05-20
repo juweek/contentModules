cbd67169a64a28491ac16bcad55f6ac87b650e33
juweek <juweek.adolphe@gmail.com>
2022-05-20 12:01:42 -0400

    changing image

--- buttonChangeMap/js/main.js
+++ buttonChangeMap/js/main.js
@@ -3,19 +3,38 @@
 METHOD: set the size of the canvas
 ------------------------------
 */
-var coll = document.getElementsByClassName("collapsible");
-var i;
+var svgElement = document.getElementById("treeMapContainer");
+var buttonElements = document.querySelectorAll(".buttonsContainer button");
+var chartElements = document.querySelectorAll("#treeMapContainer > div");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
+/*
+------------------------------
+METHOD: button click interaction
+------------------------------
+*/
+
+buttonElements.forEach((element) =>
+  element.addEventListener("click", (event) => {
+    buttonElements.forEach((button) => {
+      button.classList.remove("active");
+    });
+
+    let currentFilter = event.target.dataset.filter
+
+    chartElements.forEach((chart) => {
+      if (currentFilter == chart.dataset.chart) {
+        chart.classList.add("active");
+      }
+      else {
+        chart.classList.remove("active");
+      }
+    });
+
+    event.target.classList.add("active");
+
+    svgElement.classList = "";
+    svgElement.classList.add(event.target.dataset.filter);
+  })
+);
 
 //create a row of three HTML buttons 
